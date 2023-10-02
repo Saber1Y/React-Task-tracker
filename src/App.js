ebcda4db5b8d10f6a51c92a8b5203ef6ea2940f6
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-02 15:49:55 +0100

    CREATE: delete functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import Header from "./components/Header";
-import './App.scss';
+import { useState } from "react"
+import Header from "./components/Header"
+import './App.scss'
+import Tasks from "./components/Tasks"
 
-function App() {
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'feb 25th 2022',
+        
+    },
+    {
+        id: 2,
+        text: 'Meetings at school',
+        day: 'feb 5th 2021',
+        
+    },
+    {
+        id: 3,
+        text: 'food shopping',
+        day: 'feb 15th 2012',
+       
+    },
+])
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
   return (
     <div className="container">
     <Header  />
+    {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete=
+      {deleteTask} />
+     ) : (
+      'No Tasks to show'
+      )}
     </div>
-  );
-}
-
+  )
+}  
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,14 @@ import PropTypes from 'prop-types'
 
 
 const Header = ({ title }) => {
+
+    const onCLick = () => {
+        console.log('click')
+    }
   return (
-    <header>
+    <header className='header'>
         <h1> {title} </h1>
+        <button className='btn' onClick={onCLick} >Add</button>
     </header>
   )
 }
